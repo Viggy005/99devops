e77bca3eab68a625774f6a1e5a0529c4382d6ccb
Venkatesh vivekandhan <venkateshvivekandhan@venkateshs-air.mynet>
2021-11-30 13:21:48 +0000

    app is runningwhen we enter link localhost:8080

--- starter-code/provision.sh
+++ starter-code/provision.sh
@@ -5,5 +5,5 @@ sudo apt-get install nginx -y
 systemctl status nginx
 sudo apt-get install nodejs -y
 curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
-sudo apt-get install nodejs
+sudo apt-get install nodejs -y
 sudo npm install pm2 -g
