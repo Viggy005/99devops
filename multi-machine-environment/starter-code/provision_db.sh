a464875e424e5a951df48bfa429d08a705c7f356
Venkatesh vivekandhan <venkateshvivekandhan@venkateshs-air.mynet>
2021-11-30 20:50:53 +0000

    ip/posts works: connected db and app vm's by creating a environment variable

--- multi-machine-environment/starter-code/provision_db.sh
+++ multi-machine-environment/starter-code/provision_db.sh
@@ -7,9 +7,14 @@ sudo apt-get install nodejs -y
 curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
 sudo apt-get install nodejs -y
 sudo npm install pm2 -g
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 9DA31620334BD75D9DCB49F368818C72E52529D4
-sudo add-apt-repository 'deb [arch=amd64] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.0 multiverse'
-sudo apt update
-sudo apt install mongodb-org -y
+wget -qO - https://www.mongodb.org/static/pgp/server-4.4.asc | sudo apt-key add -
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.4.list
+sudo apt-get update
+sudo apt-get install -y mongodb-org
 sudo systemctl start mongod
+sudo systemctl status mongod
 sudo systemctl enable mongod
+sudo ufw allow 27017
+
+
+
